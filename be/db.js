b94b6594ccfdab61791322a4ef241d1738e5e588
B FAROOQ <bfarooq2022@gmail.com>
2024-12-01 13:02:50 +0530

    files here

---
+++ be/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("http://localhost:27017/todos");
+
+//schema 
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+//model to export
+const todoModel = mongoose.model('todos', todoSchema);
+
+module.exports = {
+    todoModel
+}
